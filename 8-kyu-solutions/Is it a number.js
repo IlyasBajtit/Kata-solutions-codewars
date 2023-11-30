30627a853f1bf4fa2dbe0b32e62bb562bb9078e2
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-11-30 18:16:48 +0100

    update

--- 8-kyu-solutions/Is it a number.js
+++ 8-kyu-solutions/Is it a number.js
@@ -1,6 +1,6 @@
 /**Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
 
-Valid examples, should return true:
+vvalid examples, should return true:
 
 isDigit("3")
 isDigit("  3  ")
