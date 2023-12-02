daf647b2df36ce3b68f3fd3bf9fa42031f1edaab
IlyasBajtit <ilyas.bajtit@gmail.com>
2023-12-02 22:50:18 +0100

    update

--- 8-kyu-solutions/Grasshopper - Personalized Message.js
+++ 8-kyu-solutions/Grasshopper - Personalized Message.js
@@ -6,7 +6,7 @@ Use conditionals to return the proper message:
 
 case	              return
 name equals owner 	'Hello boss'
-otherwise 	         'ello guest' */
+otherwise 	         'Hello guest' */
 
 function greet (name, owner) {
     return `Hello ${name==owner?'boss':'guest'}`
